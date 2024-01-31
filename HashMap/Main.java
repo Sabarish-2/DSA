d1847c78e18101cb4c6fafc8da7a92a5cc53a016
Sabarish-2 <sabarishh03@gmail.com>
2024-01-31 17:27:02 +0530

    Update MyHashMap class

--- HashMap/Main.java
+++ HashMap/Main.java
@@ -2,7 +2,19 @@ package HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        MyHashMap map = new MyHashMap();
+        MyHashMap map = new MyHashMap(3);
         map.put(1, "Hello");
+        map.put(21, "Hello");
+        map.put(31, "Hello");
+        map.put(1, "Bye");
+        map.put(3, "World");
+        map.put(61, "!");
+        map.put(8, "Hello");
+        System.out.println(map);
+        System.out.println(map.get(21) + " " + map.getOrDefault(31, "World") + map.get(61));
+        // System.out.println(map);
+        System.out.println(map.get(22));
+        System.out.println(map.remove(31) + map.remove(61) + map.remove(1));
+        System.out.println(map);
     }
 }
--- HashMap/MyHashMap.java
+++ HashMap/MyHashMap.java
@@ -23,19 +23,91 @@ public class MyHashMap {
 
     MyHashMap (int n) {
         size = n;
-        list = new LinkedList<>();
+        this.list = new LinkedList[n];
+    }
+
+    String getOrDefault(int key, String value)
+    {
+        String val = get(key);
+        if (val == null) return value;
+        return val;
+    }
+
+    String remove(int key)
+    {
+        if (get(key) == null) return null;
+        int k = hashKey(key);
+        Node node = list[k].get(0);
+        if (node.key == key)
+        {
+            return list[k].remove().value;
+        }
+        while (node.next.key != key)
+        {
+            node = node.next;
+        }
+        String val = node.next.value;
+        node.next = node.next.next;
+        return val;
     }
 
     int hashKey(int key) {
         return key % size;
     }
 
-    void put (int key, String val) {
+    void put(int key, String val) {
         int k = hashKey(key);
+        Node newNode = new Node(key, val);
         if (list[k] == null)
         {
-            list[k] = new LinkedList<>();
+            list[k] = new LinkedList<Node>();
+            list[k].add(newNode);
+            return;
+        }
+        Node node = list[k].get(0);
+        while (node.key != key && node.next != null)
+        {
+            node = node.next;
+        } 
+        if (node.key == key) node.value = val;
+        else
+        {
+            node.next = newNode;
         }
     }
     
+    String get(int key)
+    {
+        int k = hashKey(key);
+        if (list[k] == null) return null;
+        Node node = list[k].get(0);
+        while (node.key != key && node.next != null)
+            node = node.next;
+        if (node.key == key) return node.value;
+        return null;
+    }
+    
+    public String toString() {
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (LinkedList<Node> lList : list) {
+            if(lList != null && !lList.isEmpty()) sb.append(listPrint(lList)).append(", ");
+        }
+        sb.append("\b\b}");
+        return sb.toString();
+    }
+
+    String listPrint(LinkedList<Node> lList) {
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+        Node node = lList.get(0);
+        while (node != null)
+        {
+            s.append(node.key).append(" : ").append(node.value).append(", ");
+            node = node.next;
+        }
+        s.append("\b\b]");
+        return s.toString();
+    }
 }
