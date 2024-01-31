8a727cfcec21a6d38fbf5b7d4db6caa78173ffe6
Sabarish-2 <118958969+Sabarish-2@users.noreply.github.com>
2024-01-31 06:39:20 +0000

    HashKey

--- HashMap/MyHashMap.java
+++ HashMap/MyHashMap.java
@@ -1,8 +1,39 @@
 package HashMap;
 
+class Node {
+    int key;
+    String value;
+    Node next;
+    Node (int key, String val) {
+        this.key = key;
+        this.value = val;
+    }
+}
+
 /**
  * MyHashMap
  */
 public class MyHashMap {
+
+    Node head;
+    int size;
+    LinkedList<Node>[] list;
+
+    MyHashMap (int n) {
+        size = n;
+        list = new LinkedList<>()[size];
+    }
+
+    int hashKey(int key) {
+        return key % size;
+    }
+
+    void put (int key, String val) {
+        int k = hashKey(key);
+        if (list[k] == null)
+        {
+            list[k] = new LinkedList<>();
+        }
+    }
     
 }
